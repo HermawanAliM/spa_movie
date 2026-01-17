00f4fc2d2137805e5c3540d37b02a789e4681427
HermawanAliM <hermawanali359@gmail.com>
2026-01-17 14:54:29 +0700

    menambahkan routing navigasi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-  
-    </>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/movie/:id" element={<Detail/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
